242c8660128ac3b6b5419667f0097a615bdeed2e
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-19 15:31:54 +0100

    Add title

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    
+  },
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+
+<template>
+<app-title></app-title>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'Title',
+    data() {
+        return {
+            
+        }
+    }
+}
+
+</script>
+
+
+<template>
+<h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+
+</style>
